3d035b1d07fae30d33333d58019089984d5e2921
parsamoh <parsamohammadnezhad@gmail.com>
2025-05-17 15:18:16 +0330

    update scripts

---
+++ get_cert.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+read -rp "Enter DOMAIN: " DOMAIN_NAME
+certbot certonly --standalone --register-unsafely-without-email --non-interactive --agree-tos -d DOMAIN_NAME
--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 
 curl -fsSL https://get.docker.com | sh
 curl -fsSL https://sing-box.app/install.sh | sh -s -- --beta
+snap install core
+snap refresh core
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
